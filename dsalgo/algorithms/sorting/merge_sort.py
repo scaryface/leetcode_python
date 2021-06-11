34c7ff712e585d9b81a54d4be0330f1a71d026d7
scaryface <adityakanthale@gmail.com>
2021-06-11 15:44:47 +0530

    refactoring file structure

--- dsalgo/algorithms/sorting/merge_sort.py
+++ dsalgo/algorithms/sorting/merge_sort.py
@@ -1,8 +1,5 @@
 class MergeSort:
-    def __init__(self):
-        return
-
-    @staticmethod
+    
     def sort(self, A, key=lambda x: x, reverse=False):
         B, K = A[:], [key(a) for a in A]
         self.sort_with_index(B, K, 0, len(B)-1, reverse)
@@ -12,29 +9,38 @@ class MergeSort:
         if l == r:
             return B
         m = l + (r-l)//2
-        self.sort_with_index(B, K, 0, m)
-        self.sort_with_index(B, K, m+1, r)
+        self.sort_with_index(B, K, 0, m, reverse=reverse)
+        self.sort_with_index(B, K, m+1, r, reverse=reverse)
         return self.merge(B, K, l, m, r, reverse)
 
     def merge(self, B, K, l, m, r, reverse):
         l_ptr, r_ptr, curr_ptr = l, m+1, l
-        C = B[l:r+1]
+        C = B[:]
+        CK = K[:]
+        # print('=================')
+        # print(B[l:m+1], B[m+1:r+1])
         while l_ptr <= m and r_ptr <= r:
-            if K[l_ptr] <= K[r_ptr]:
+            #print(l_ptr, r_ptr, curr_ptr)
+            if CK[l_ptr] <= CK[r_ptr]:
                 B[curr_ptr] = C[l_ptr]
+                K[curr_ptr] = CK[l_ptr]
                 l_ptr += 1
-            elif K[l_ptr] > K[r_ptr]:
+            elif CK[l_ptr] > CK[r_ptr]:
                 B[curr_ptr] = C[r_ptr]
+                K[curr_ptr] = CK[r_ptr]
                 r_ptr += 1
             curr_ptr += 1
         while l_ptr <= m:
             B[curr_ptr] = C[l_ptr]
+            K[curr_ptr] = CK[l_ptr]
             l_ptr += 1
             curr_ptr += 1
         while r_ptr <= r:
             B[curr_ptr] = C[r_ptr]
+            K[curr_ptr] = CK[r_ptr]
             r_ptr += 1
             curr_ptr += 1
+        #print(B[l:r+1])
 
 
             
--- tests/algorithms/sorting/test_merge_sort.py
+++
@@ -1,8 +0,0 @@
-from algorithms.sorting import MergeSort
-
-def merge_sort_test():
-    MergeSort.sort([3,4,1,9,7,6])
-
-if __name__ == '__main__':
-    merge_sort_test()
-
